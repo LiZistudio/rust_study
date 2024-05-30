0d879ece7dac92959e4861dd3a94eccab3c1aadb
LiZistudio <1185345172@qq.com>
2024-05-30 12:41:51 +0800

    reference And borrow

---
+++ reference_borrow/target/debug/deps/reference_borrow-be4b2f22f440f770.d
@@ -0,0 +1,5 @@
+D:\rust_study\reference_borrow\target\debug\deps\libreference_borrow-be4b2f22f440f770.rmeta: src\main.rs
+
+D:\rust_study\reference_borrow\target\debug\deps\reference_borrow-be4b2f22f440f770.d: src\main.rs
+
+src\main.rs:
---
+++ reference_borrow/target/debug/deps/reference_borrow.d
@@ -0,0 +1,5 @@
+D:\rust_study\reference_borrow\target\debug\deps\reference_borrow.exe: src\main.rs
+
+D:\rust_study\reference_borrow\target\debug\deps\reference_borrow.d: src\main.rs
+
+src\main.rs:
