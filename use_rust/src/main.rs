7f8be511ae14081f31af23b5f025b6fce5f888d6
LiZistudio <1185345172@qq.com>
2024-09-26 13:07:41 +0800

    rust_use

--- use_rust/src/main.rs
+++ use_rust/src/main.rs
@@ -1,7 +1,18 @@
-
+use std::collections::HashMap;
+//use std::{fmt,io};
 
 fn main() {
     eat_at_restaurant("扣三丝", "李华", 0001);
+    let mut map:HashMap<i32,i32> = HashMap::new();
+    map.insert(1,2);
+    println!("map:{:?}",map);
+
+    //fn function1 () -> fmt::Result {}
+    //fn function2 () -> io::Result<()> {}
+
+    let result = my_function(3,4);
+    println!("map2:{:?}",result);
+
 }
 
 
@@ -35,4 +46,14 @@ pub fn eat_at_restaurant (order_name:&str,name:&str,num:u8) {
     hosting::Custom::add_to_waitlist(name,num);
 
     println!("您的客户点的菜是{:?}。",order_name);
+}
+
+
+//使用 as 关键字提供新的名称
+use std::collections::HashMap as MyHashMap;
+
+fn my_function (a:i32,b:i32) -> MyHashMap<i32,i32> {
+    let mut map2:MyHashMap<i32,i32> = MyHashMap::new();
+    map2.insert(a, b);
+    map2
 }
