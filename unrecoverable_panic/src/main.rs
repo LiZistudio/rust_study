d28f5c936ed6ba483d5bf41f3805fe932772efc1
LiZistudio <1185345172@qq.com>
2024-10-22 12:20:05 +0800

    unrecoverable and panic finished

--- unrecoverable_panic/src/main.rs
+++ unrecoverable_panic/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    println!("unrecoverable and panic!");
+    //panic!("crash and burn");
+
+
+    //使用panic！的backtrace
+    let v1 = vec![1,2,3,4,5,6,7];
+    v1[99];
+
+    
 }
---
+++ unrecoverable_panic/target/debug/unrecoverable_panic.d
@@ -0,0 +1,1 @@
+D:\rust_study\unrecoverable_panic\target\debug\unrecoverable_panic.exe: D:\rust_study\unrecoverable_panic\src\main.rs
