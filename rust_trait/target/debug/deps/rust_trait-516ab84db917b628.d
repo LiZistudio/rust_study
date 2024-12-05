4a2ee94a03eddccae3e3c17fc37596978d6cf118
LiZistudio <1185345172@qq.com>
2024-12-05 13:07:45 +0800

    clean

--- rust_trait/target/debug/deps/rust_trait-516ab84db917b628.d
+++
@@ -1,5 +0,0 @@
-D:\rust_study\rust_trait\target\debug\deps\librust_trait-516ab84db917b628.rmeta: src/main.rs
-
-D:\rust_study\rust_trait\target\debug\deps\rust_trait-516ab84db917b628.d: src/main.rs
-
-src/main.rs:
--- rust_trait/target/debug/deps/rust_trait-74d1292ab4aedfed.d
+++
@@ -1,5 +0,0 @@
-D:\rust_study\rust_trait\target\debug\deps\librust_trait-74d1292ab4aedfed.rmeta: src/main.rs
-
-D:\rust_study\rust_trait\target\debug\deps\rust_trait-74d1292ab4aedfed.d: src/main.rs
-
-src/main.rs:
