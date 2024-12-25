5cf97d15fa19d4ca12ee7f1bdbe65f274c5a7fd9
LiZistudio <1185345172@qq.com>
2024-12-25 11:25:31 +0800

    rust_test clean

--- rust_test/src/main.rs
+++ rust_test/src/main.rs
@@ -9,6 +9,34 @@ Rust 中的测试函数是用来验证非测试代码是否按照期望的方式
 
 //测试函数剖析
 
+use std::{
+    fs::File,
+    io::Write,
+    os::windows::fs::FileExt,
+};
+
+    
 fn main() {
     println!("Rust 编写自动化测试");
+    create_file_success();    
+}
+
+fn create_file_success() {
+
+    let mut my_file = create_file("poem.md");
+    for i in 1..11 {
+        if i%2==0 {
+            my_file.write(b"\n# To be or not to be , this is a question.")
+            .expect("写入失败");
+        }else {
+            //my_file.write(b"\n## To be or not to be , this is a question.").expect("写入失败");
+            my_file.seek_write(b" ",0)
+            .expect("写入失败");
+        }
+    }
+}
+
+pub fn create_file(name:&str) -> File {
+    let file = File::create(name).expect("创建文件失败");
+    file
 }
