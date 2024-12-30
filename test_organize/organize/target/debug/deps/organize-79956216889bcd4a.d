d8fbc7b72fca5c2a3c74961309e66285023dd007
LiZistudio <1185345172@qq.com>
2024-12-30 17:59:09 +0800

    test_organize v0.8

---
+++ test_organize/organize/target/debug/deps/organize-79956216889bcd4a.d
@@ -0,0 +1,5 @@
+D:\rust_study\test_organize\organize\target\debug\deps\liborganize-79956216889bcd4a.rmeta: src\lib.rs
+
+D:\rust_study\test_organize\organize\target\debug\deps\organize-79956216889bcd4a.d: src\lib.rs
+
+src\lib.rs:
