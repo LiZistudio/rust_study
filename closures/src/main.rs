8b28bed7c70b237eb93972cd5d9d3a1f45b1ccea
LiZistudio <1185345172@qq.com>
2025-01-25 17:53:02 +0800

    closures v0.4

--- closures/src/main.rs
+++ closures/src/main.rs
@@ -16,35 +16,153 @@ fn main() {
         simulated_user_specified_value,
         simulated_random_number
     );
+
+    //示例 13-8：尝试调用一个被推断为两个不同类型的闭包
+    let example_closure = |x| x;
+    let result = example_closure(String::from("杨柳岸，晓风残月。多情应笑我，早生华发。"));
+    println!("examplr_closure's value is :{}",result);
+    //example_closure(3);
+    //第一次使用 String 值调用 example_closure 时，编译器推断 x 和此闭包返回值的类型为 String。
+    //接着这些类型被锁定进闭包 example_closure 中，如果尝试对同一闭包使用不同类型则会得到类型错误。
+
 }
 
 
 // /使用闭包创建行为的抽象
 //示例 13-1：一个用来代替假定计算的函数，它大约会执行两秒钟
-fn simulated_expensive_calculation(intensity:u32) -> u32 {
+fn _simulated_expensive_calculation(intensity:u32) -> u32 {
     println!("calculating slowly...");
     thread::sleep(Duration::from_secs(2));
     intensity
 }
 
 //示例 13-3：程序的业务逻辑，它根据输入并调用 simulated_expensive_calculation 函数来打印出健身计划
+// fn generate_workout(intensity:u32,random_number:u32) {
+//     if intensity < 25 {
+//         println!(
+//             "Today, do {} pushups!",
+//             simulated_expensive_calculation(intensity)
+//         );
+//         println!(
+//             "Next, do {} situps!",
+//             simulated_expensive_calculation(intensity)
+//         );
+//     } else if random_number == 3 {
+//             println!("Take a break today! Remember to stay hydrated!");
+//         } else {
+//             println!(
+//                 "Today, run for {} minutes!",
+//                 simulated_expensive_calculation(intensity)
+//             );
+//             println!("random:{}",random_number);
+//         }
+// }
+
+
+//使用函数重构
+//示例 13-4：将 simulated_expensive_calculation 调用提取到一个位置，并将结果储存在变量 expensive_result 中
+// fn generate_workout(intensity:u32,random_number:u32) {
+//     let expensive_result = simulated_expensive_calculation(intensity);
+//     if intensity < 25 {
+//         println!(
+//             "Today, do {} pushups!",
+//             expensive_result
+//         );
+//         println!(
+//             "Next, do {} situps!",
+//             expensive_result
+//         );
+//     } else if random_number == 3 {
+//             println!("Take a break today! Remember to stay hydrated!");
+//         } else {
+//             println!(
+//                 "Today, run for {} minutes!",
+//                 expensive_result
+//             );
+//             println!("random:{}",random_number);
+//         }
+// }
+
+
+//重构使用闭包储存代码
 fn generate_workout(intensity:u32,random_number:u32) {
+
+    // //示例 13-5：定义一个闭包并储存到变量 expensive_closure 中
+    // let expensive_closures = |num| {
+    //     println!("calculating slowly...");
+    //     thread::sleep(Duration::from_secs(2));
+    //     num
+    // };
+
+    //示例 13-11：在 generate_workout 函数中利用 Cacher 结构体来抽象出缓存逻辑
+    let mut expensive_result = Cacher::new(|num| {
+        println!("calculating slowly...");
+        thread::sleep(Duration::from_secs(2));
+        num
+    });
+
+
+    //示例 13-7：为闭包的参数和返回值增加可选的类型标注
+    // let expensive_closures = |num:u32| -> u32 {
+    //     println!("calculating slowly...");
+    //     thread::sleep(Duration::from_secs(2));
+    //     num
+    // };
+
     if intensity < 25 {
         println!(
             "Today, do {} pushups!",
-            simulated_expensive_calculation(intensity)
+            expensive_result.value(intensity)
         );
         println!(
             "Next, do {} situps!",
-            simulated_expensive_calculation(intensity)
+            expensive_result.value(intensity)
+
         );
     } else if random_number == 3 {
             println!("Take a break today! Remember to stay hydrated!");
         } else {
             println!(
                 "Today, run for {} minutes!",
-                simulated_expensive_calculation(intensity)
+                expensive_result.value(intensity)
+
             );
             println!("random:{}",random_number);
         }
+}
+//示例 13-6：调用定义的 expensive_closure
+
+//************使用带有泛型和 Fn trait 的闭包**********************
+//示例 13-9：定义一个 Cacher 结构体来在 calculation 中存放闭包并在 value 中存放 Option 值
+struct Cacher<T> 
+    where T:Fn(u32) -> u32 
+{
+    calculation:T,
+    value:Option<u32>,
+}
+
+impl<T> Cacher<T> 
+    where T:Fn(u32) -> u32
+{
+    fn new(calculation:T) -> Cacher<T> {
+        Cacher {
+            calculation,
+            value:None,
+        }
     }
+
+    fn value(&mut self,arg:u32) -> u32 {
+            match self.value {
+                Some(v) => v,
+                None => {
+                    let v = (self.calculation) (arg);
+                    self.value = Some(v);
+                    v
+                },
+            }
+        }
+}
+//示例 13-10：Cacher 的缓存逻辑
+
+
+//Cacher实现的限制
