dbaa0aa4f105bc4ca59b84004c799069af8233bb
LiZistudio <1185345172@qq.com>
2025-02-10 12:11:28 +0800

    closures v0.9

--- closures/src/main.rs
+++ closures/src/main.rs
@@ -174,11 +174,11 @@ mod tests {
 
     #[test]
     fn call_with_different_values() {
-        let mut c = Cacher::new(|a| a);
+        let mut c = Cacher::new(|a| {let result = (a+1)*10;result});
     
         let _v1 = c.value(1);
         let v2 = c.value(2);
     
-        assert_eq!(v2, 2);
+        assert_eq!(v2, 30);
     }
 }
