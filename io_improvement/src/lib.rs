4a1a3f40438e89db0cebda901535efb03647da4f
LiZistudio <1185345172@qq.com>
2025-02-26 23:24:44 +0800

    io_improvement v0.1.3

--- io_improvement/src/lib.rs
+++ io_improvement/src/lib.rs
@@ -22,47 +22,66 @@ pub struct Config {
 }
 //解析参数
 impl Config {
-    pub fn new(args:&[String]) -> Result<Config,&'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
+    
+    //示例 13-26：以迭代器作为参数更新 Config::new 的签名
+    pub fn new(mut args:std::env::Args) -> Result<Config,&'static str> {
+        args.next();
 
-        let query = args[1].clone();
-        let filename = args[2].clone();
+        //使用 Iterator trait 代替索引
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+        let filename = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file name"),
+        };
         let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
     
         //示例 12-23：检查叫做 CASE_INSENSITIVE 的环境变量
         Ok(Config{query,filename,case_sensitive})
     
     }
-}
+}//示例 13-27：修改 Config::new 的函数体来使用迭代器方法
+
 
 //search
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
+    // let mut results = Vec::new();
 
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
+    // for line in contents.lines() {
+    //     if line.contains(query) {
+    //         results.push(line);
+    //     }
+    // }
 
-    results
-}
+    // results
+
+    contents.lines()
+    .filter(|line| line.contains(query))
+    .collect()
+
+}//示例 13-29：在 search 函数实现中使用迭代器适配器
+
 
 //实现 search_case_insensitive 函数
 pub fn search_case_insensitive<'a>(query: &str,contents: &'a str) -> Vec<&'a str> {
-    let query = query.to_lowercase();
-    let mut results:Vec<&str> = Vec::new();
+    //let query = query.to_lowercase();
+    // let mut results:Vec<&str> = Vec::new();
 
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query[..]) {
-            results.push(line);
-        }
-    }
-    results
+    // for line in contents.lines() {
+    //     if line.to_lowercase().contains(&query[..]) {
+    //         results.push(line);
+    //     }
+    // }
+    // results
+
+    contents.lines()
+    .filter(|line| line.to_lowercase().contains(&query.to_lowercase()))
+    .collect()
 }
 //示例 12-21：定义 search_case_insensitive 函数，它在比较查询和每一行之前将他们都转换为小写
+
 
 #[cfg(test)]
 mod test {
