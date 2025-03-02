f5d6310945ccab0121e7c9e1771aaaae40f5322b
LiZistudio <1185345172@qq.com>
2025-03-02 23:59:33 +0800

    io_improvement v0.1.5

--- iterator_loop/src/main.rs
+++ iterator_loop/src/main.rs
@@ -7,6 +7,17 @@ fn main() {
 mod tests {
     #[test]
     fn iterator_loop() {
-        
+        let buffer: &mut [i32] = 70;
+        let coefficients: [i64; 12] = [3;12];
+        let qlp_shift: i16 = 30;
+
+        for i in 12..buffer.len() {
+            let prediction = coefficients.iter()
+                                        .zip(&buffer[i - 12..i])
+                                        .map(|(&c, &s)| c * s as i64)
+                                        .sum::<i64>() >> qlp_shift;
+            let delta = buffer[i];
+            buffer[i] = prediction as i32 + delta;
+        }
     }
 }
