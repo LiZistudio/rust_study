831422d0d2cecc45afda35cc1b14c53c00ac1678
LiZistudio <1185345172@qq.com>
2025-03-04 01:14:09 +0800

    release_profile_config v0.2.1

--- release_profiles_config/src/main.rs
+++ release_profiles_config/src/main.rs
@@ -1,36 +1,64 @@
 
 slint::slint!{
+    import { Button } from "std-widgets.slint";
+
     export component MainWindow inherits Window {
         title: "留白";
-        min-height: 500px;
+        min-height: 600px;
         min-width: 900px;
         VerticalLayout {
-            Rectangle {
-                TextInput {
-                    horizontal-alignment: center;
-                    vertical-alignment: center;
-                    text: "陌上花开，可缓缓归矣。";
-                    color: lightgrey;
-                    font-size: 50px;
-                    font-family: "Microsoft YaHei";
+            HorizontalLayout {
+                Rectangle {
+                    height: 20px;
+                    width: 50px;
+                    drop-shadow-blur: 20px;
+                    drop-shadow-color: lightgrey;
+                    Button {
+                        text: "文件";
+                    }
+                }
+                Rectangle {
+                    height: 20px;
+                    width: 50px;
+                    drop-shadow-blur: 20px;
+                    drop-shadow-color: lightgrey;
+                    Button {
+                        text: "编辑";
+                    }
+                }
+                Rectangle {
+                    height: 20px;
+                    width: 50px;
+                    drop-shadow-blur: 20px;
+                    drop-shadow-color: lightgrey;
+                    Button {
+                        text: "帮助";
+                    }
                 }
             }
-            // Button {
-            //     text: "点击我";
-            //     width: 120px;
-            //     height: 40px;
-            //     background: #4CAF50;
-            //     border-radius: 4px;
-            //     color: white;
-            //     font-size: 16px;
-                
-            //     clicked => {
-            //         debug("按钮被点击了");
-            //     }
-            // }
+            Rectangle {
+                Text {
+                    horizontal-alignment: center;
+                    vertical-alignment: center;
+                    text: "都门帐饮无绪，\n杨柳岸，晓风残月。";
+                    color: lightgrey;
+                    font-size: 70px;
+                    font-family: "Songti SC";
+                }
+            }
+            Rectangle {
+                height: 40px;
+                width: 70px;
+                drop-shadow-blur: 20px;
+                drop-shadow-color: lightgrey;
+                Button {
+                    text: "↻刷新";
+                }
+            }
         }
     }
 }
+
 fn main() {
     println!("release_profile_config");
     MainWindow::new().unwrap().run().unwrap();
