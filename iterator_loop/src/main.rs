92e890821bf540c494fc3b31e17429a8ac9f8bba
LiZistudio <1185345172@qq.com>
2025-03-03 14:33:04 +0800

    iterator_loop v0.1.9

--- iterator_loop/src/main.rs
+++ iterator_loop/src/main.rs
@@ -15,7 +15,7 @@ mod tests {
             let prediction = coefficients.iter()
                                         .zip(&buffer[i - 12..i])
                                         .map(|(&c, &s)| c * s as i64)
-                                        .sum::<i64>() >> qlp_shift;
+                                        .sum::<i64>() >> qlp_shift;//右移30位->除以2^30
             let delta = buffer[i];
             buffer[i] = prediction as i32 + delta;
         }
