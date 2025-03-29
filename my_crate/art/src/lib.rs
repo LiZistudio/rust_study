8c5789bd341e12df1236ac3a4808991a1428a91c
shucai <guanxinliangshiheshucai@outlook.com>
2025-03-29 22:25:50 +0800

    my_crate v1.1.3

--- my_crate/art/src/lib.rs
+++ my_crate/art/src/lib.rs
@@ -5,6 +5,8 @@ pub mod kinds {
         Blue,
     }
 
+    #[derive(PartialEq)]
+    #[derive(Debug)]
     pub enum SecondaryColor {
         Orange,
         Green,
@@ -16,13 +18,15 @@ pub mod utils {
     use crate::kinds::*;
 
     pub fn mix(c1: PrimaryColor, c2: PrimaryColor) -> SecondaryColor {
-        match (c1, c2) {
+        let tup = (c1, c2);
+        match tup {
             (PrimaryColor::Red, PrimaryColor::Yellow) => SecondaryColor::Orange,
             (PrimaryColor::Yellow, PrimaryColor::Red) => SecondaryColor::Orange,
             (PrimaryColor::Red, PrimaryColor::Blue) => SecondaryColor::Purple,
             (PrimaryColor::Blue, PrimaryColor::Red) => SecondaryColor::Purple,
             (PrimaryColor::Yellow, PrimaryColor::Blue) => SecondaryColor::Green,
             (PrimaryColor::Blue, PrimaryColor::Yellow) => SecondaryColor::Green,
+            _ => panic!("Don't need to mix the same color"),
         }
     }
 }
