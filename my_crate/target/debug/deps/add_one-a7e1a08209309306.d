27a1a7756b3f7f902f641746dffcf73edb2e3dae
shucai <guanxinliangshiheshucai@outlook.com>
2025-09-11 01:09:09 +0800

    my_crate finish

--- my_crate/target/debug/deps/add_one-a7e1a08209309306.d
+++
@@ -1,5 +0,0 @@
-/Users/kk/Desktop/rust_study/my_crate/target/debug/deps/add_one-a7e1a08209309306.d: add_one/src/lib.rs
-
-/Users/kk/Desktop/rust_study/my_crate/target/debug/deps/libadd_one-a7e1a08209309306.rmeta: add_one/src/lib.rs
-
-add_one/src/lib.rs:
--- my_crate/target/debug/deps/my_crate-4b39203ee96c5df2.d
+++
@@ -1,5 +0,0 @@
-/Users/kk/Desktop/rust_study/my_crate/target/debug/deps/my_crate-4b39203ee96c5df2.d: src/main.rs
-
-/Users/kk/Desktop/rust_study/my_crate/target/debug/deps/libmy_crate-4b39203ee96c5df2.rmeta: src/main.rs
-
-src/main.rs:
